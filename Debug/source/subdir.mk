b2104cdead86c7fc47c0d294aa3ec25caba95c7b
OswaldoReyesGithub <oswaldo.reyes@iteso.mx>
2024-09-26 14:00:06 -0600

    LibreriaAes128Agregada

---
+++ Debug/source/subdir.mk
@@ -0,0 +1,37 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../source/aes.c \
+../source/lwip_tcpecho_freertos.c \
+../source/semihost_hardfault.c 
+
+C_DEPS += \
+./source/aes.d \
+./source/lwip_tcpecho_freertos.d \
+./source/semihost_hardfault.d 
+
+OBJS += \
+./source/aes.o \
+./source/lwip_tcpecho_freertos.o \
+./source/semihost_hardfault.o 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+source/%.o: ../source/%.c source/subdir.mk
+	@echo 'Building file: $<'
+	@echo 'Invoking: MCU C Compiler'
+	arm-none-eabi-gcc -std=gnu99 -D__REDLIB__ -DCPU_MK64FN1M0VLL12 -DCPU_MK64FN1M0VLL12_cm4 -DUSE_RTOS=1 -DPRINTF_ADVANCED_ENABLE=1 -DFRDM_K64F -DFREEDOM -DLWIP_DISABLE_PBUF_POOL_SIZE_SANITY_CHECKS=1 -DSERIAL_PORT_TYPE_UART=1 -DSDK_OS_FREE_RTOS -DMCUXPRESSO_SDK -DSDK_DEBUGCONSOLE=1 -DCR_INTEGER_PRINTF -DPRINTF_FLOAT_ENABLE=0 -D__MCUXPRESSO -D__USE_CMSIS -DDEBUG -I"C:\Users\tiori\Documents\MCUXpressoIDE_11.10.0_3148\workspace\Practica1_ReyesRamirezOswaldo_tx_rx_enettransfer\source" -I"C:\Users\tiori\Documents\MCUXpressoIDE_11.10.0_3148\workspace\Practica1_ReyesRamirezOswaldo_tx_rx_enettransfer\mdio" -I"C:\Users\tiori\Documents\MCUXpressoIDE_11.10.0_3148\workspace\Practica1_ReyesRamirezOswaldo_tx_rx_enettransfer\phy" -I"C:\Users\tiori\Documents\MCUXpressoIDE_11.10.0_3148\workspace\Practica1_ReyesRamirezOswaldo_tx_rx_enettransfer\lwip\contrib\apps\tcpecho" -I"C:\Users\tiori\Documents\MCUXpressoIDE_11.10.0_3148\workspace\Practica1_ReyesRamirezOswaldo_tx_rx_enettransfer\lwip\port" -I"C:\Users\tiori\Documents\MCUXpressoIDE_11.10.0_3148\workspace\Practica1_ReyesRamirezOswaldo_tx_rx_enettransfer\lwip\src" -I"C:\Users\tiori\Documents\MCUXpressoIDE_11.10.0_3148\workspace\Practica1_ReyesRamirezOswaldo_tx_rx_enettransfer\lwip\src\include" -I"C:\Users\tiori\Documents\MCUXpressoIDE_11.10.0_3148\workspace\Practica1_ReyesRamirezOswaldo_tx_rx_enettransfer\drivers" -I"C:\Users\tiori\Documents\MCUXpressoIDE_11.10.0_3148\workspace\Practica1_ReyesRamirezOswaldo_tx_rx_enettransfer\utilities" -I"C:\Users\tiori\Documents\MCUXpressoIDE_11.10.0_3148\workspace\Practica1_ReyesRamirezOswaldo_tx_rx_enettransfer\device" -I"C:\Users\tiori\Documents\MCUXpressoIDE_11.10.0_3148\workspace\Practica1_ReyesRamirezOswaldo_tx_rx_enettransfer\component\uart" -I"C:\Users\tiori\Documents\MCUXpressoIDE_11.10.0_3148\workspace\Practica1_ReyesRamirezOswaldo_tx_rx_enettransfer\component\serial_manager" -I"C:\Users\tiori\Documents\MCUXpressoIDE_11.10.0_3148\workspace\Practica1_ReyesRamirezOswaldo_tx_rx_enettransfer\component\lists" -I"C:\Users\tiori\Documents\MCUXpressoIDE_11.10.0_3148\workspace\Practica1_ReyesRamirezOswaldo_tx_rx_enettransfer\CMSIS" -I"C:\Users\tiori\Documents\MCUXpressoIDE_11.10.0_3148\workspace\Practica1_ReyesRamirezOswaldo_tx_rx_enettransfer\freertos\freertos_kernel\include" -I"C:\Users\tiori\Documents\MCUXpressoIDE_11.10.0_3148\workspace\Practica1_ReyesRamirezOswaldo_tx_rx_enettransfer\freertos\freertos_kernel\portable\GCC\ARM_CM4F" -I"C:\Users\tiori\Documents\MCUXpressoIDE_11.10.0_3148\workspace\Practica1_ReyesRamirezOswaldo_tx_rx_enettransfer\board" -O0 -fno-common -g3 -gdwarf-4 -c -ffunction-sections -fdata-sections -ffreestanding -fno-builtin -fmerge-constants -fmacro-prefix-map="$(<D)/"= -mcpu=cortex-m4 -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -D__REDLIB__ -fstack-usage -specs=redlib.specs -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.o)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
+clean: clean-source
+
+clean-source:
+	-$(RM) ./source/aes.d ./source/aes.o ./source/lwip_tcpecho_freertos.d ./source/lwip_tcpecho_freertos.o ./source/semihost_hardfault.d ./source/semihost_hardfault.o
+
+.PHONY: clean-source
+
